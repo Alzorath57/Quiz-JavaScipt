1c5b7e8f1f57740ef8659b034f4e76f8e5cc06c2
Alzorath57 <julien.holtzinger@gmail.com>
2026-03-27 15:22:47 +0100

    Questions masqué + tableau questions

--- Script.js
+++ Script.js
@@ -1,2 +1,26 @@
 let score = 0;
 let questionActuelle = 0;
+
+function recupererQuestionsHTML() {
+  const toutesLesQuestions = [];
+
+  for (let i = 1; i < 10; i++) {
+    const questionElement = document.querySelector(`.question${i}`);
+    toutesLesQuestions.push(questionElement);
+  }
+  return toutesLesQuestions;
+}
+
+function afficherQuestion(numero) {
+  document
+    .querySelectorAll(
+      ".question1, .question2, .question3, .question4, .question5, .question6, .question7, .question8, .question9, .question10"
+    )
+    .forEach((q) => (q.style.display = "none"));
+
+  // Affiche seulement la question numero
+  document.querySelector(`.question${numero}`).style.display = "block";
+}
+
+// Démarre le quiz
+afficherQuestion(1);
